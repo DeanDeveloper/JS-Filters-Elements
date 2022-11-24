423077287aa23329bd56927fba5994d8956ab07e
Jodean Nascimento <58868894+DeanDeveloper@users.noreply.github.com>
2022-11-24 14:32:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+function filterCard({target}) {
+     let filter, cards, title;
+
+     filter = target;
+     cards = document.querySelectorAll('.cards .cards__item');
+
+     this.value != '' ?
+          (cards.forEach(card => {
+               title = card.querySelector('h3');
+
+               title = title.textContent.toLowerCase();
+               card.style.cssText = !title.includes(filter.value.toLowerCase()) ? 'display:none' : 'display:block';
+          })) : cards.forEach(card => card.style.display = 'block')
+
+}
+
+let handlerFilter = () => {
+     let input;
+     input = document.querySelector('.content__input');
+     input.addEventListener('input', filterCard);
+}
+
+handlerFilter();
